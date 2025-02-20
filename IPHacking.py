e348671ab0c974307e60d96f423fd979dd3a152c
HADIXHACKER <kalaaahoo300@gmail.com>
2025-02-20 12:18:17 +0500

    Update IPHacking.py

--- IPHacking.py
+++ IPHacking.py
@@ -43,7 +43,7 @@ def to(s):
 
 
 to(
-    f"\033[31;m Script Type>> \033[1;36mTEAM BLACK HAT Location Tool 🔥\n\033[1;31m Telegram >>\033[1;33m@oldhacker7866\n\033[31;m Creator >>\033[1;33mOLD-HACKER\n\033[31;m Tool Status >>\033[1;33mWorking\n\033[31;m Tool Value >>\033[1;33mPaid Script\n\033[31;m Time >>\033[1;33m6 month's\n\033[31;m")
+    f"\033[31;m Script Type>> \033[1;36mHADI_X_HACKER Location Tool 🔥\n\033[1;31m Telegram >>\033[1;33m@HADI_X_HACKER321\n\033[31;m Creator >>\033[1;33mHADI_X_HACKER\n\033[31;m Tool Status >>\033[1;33mWorking\n\033[31;m Tool Value >>\033[1;33mPaid Script\n\033[31;m Time >>\033[1;33m6 month's\n\033[31;m")
 print('')
 print('\033[32;m')
 
